2bd00cd540b1d49acaae2b91577b3f0cf014a5a7
hebiao <>
2022-01-12 00:42:06 +0800

    暂存

--- app/api/login.go
+++ app/api/login.go
@@ -5,7 +5,10 @@ import (
 	"g/app/model"
 	"g/library/response"
 	"github.com/dchest/captcha"
+	"github.com/gogf/gf/crypto/gmd5"
+	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
+	"github.com/techoner/gophp/serialize"
 )
 
 // 用户API管理对象
@@ -31,6 +34,29 @@ func (a *loginApi) Login(r *ghttp.Request) {
 	}
 
 	id := fmt.Sprintf("%v", r.Session.Get("captcha"))
+
+	//url := "http://test-permission.zxmn2018.com"
+	//request := "/api/checklogin"
+
+	system_key := "app_gamesystem"
+	secret := "c2711e4866767ae07061578655ccd51b"
+	param := g.MapStrStr{
+		"user_name":  loginReq.Username,
+		"password":   loginReq.Password,
+		"ip":         r.GetClientIp(),
+		"system_key": system_key,
+	}
+
+	p, err := serialize.Marshal(param)
+	if err != nil {
+		response.JsonExit(r, 1, err.Error())
+	}
+
+	pi := fmt.Sprintf("%v", p)
+	param["sign"], _ = gmd5.EncryptString(pi + secret)
+
+	fmt.Printf("%+v %s", param, p)
+
 	if !captcha.VerifyString(id, loginReq.Captcha) {
 		response.JsonExit(r, 1, "验证码错误")
 	}
