3e8eec0e073e77232f1d50adb25a326026c2aa5c
hebiao <>
2022-01-13 00:11:23 +0800

    md5加密

--- app/api/login.go
+++ app/api/login.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"bytes"
 	"fmt"
 	"g/app/model"
 	"g/library/response"
@@ -9,6 +10,7 @@ import (
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 	"github.com/techoner/gophp/serialize"
+	"sort"
 )
 
 // 用户API管理对象
@@ -40,22 +42,29 @@ func (a *loginApi) Login(r *ghttp.Request) {
 
 	system_key := "app_gamesystem"
 	secret := "c2711e4866767ae07061578655ccd51b"
-	param := g.MapStrStr{
+	paramMap := g.MapStrStr{
 		"user_name":  loginReq.Username,
 		"password":   loginReq.Password,
 		"ip":         r.GetClientIp(),
 		"system_key": system_key,
 	}
 
-	p, err := serialize.Marshal(param)
+	paramArr := g.ArrayStr{}
+	for _, v := range paramMap {
+		paramArr = append(paramArr, v)
+	}
+
+	sort.Strings(paramArr)
+
+	p, err := serialize.Marshal(paramArr)
 	if err != nil {
 		response.JsonExit(r, 1, err.Error())
 	}
 
-	pi := fmt.Sprintf("%v", p)
-	param["sign"], _ = gmd5.EncryptString(pi + secret)
+	p = bytes.Join([][]byte{p, []byte(secret)}, []byte(""))
+	paramMap["sign"], _ = gmd5.EncryptBytes(p)
 
-	fmt.Printf("%+v %s", param, p)
+	fmt.Printf("%+v %s", paramMap, p)
 
 	if !captcha.VerifyString(id, loginReq.Captcha) {
 		response.JsonExit(r, 1, "验证码错误")
